b756d8970f7d111e82df368d386addc20ba8132f
Andrea Greco <113620544+mrMart1an@users.noreply.github.com>
2025-02-26 17:34:30 +0100

    improving build script

--- scripts/buildImage.sh
+++ scripts/buildImage.sh
@@ -7,6 +7,8 @@
 WORK_DIR=${PWD}
 BUILD_DIR=${WORK_DIR}/build
 
+OUTPUT_IMG_NAME=root.img
+
 # Set up build tools environment
 source poky/oe-init-build-env ${BUILD_DIR}
 
@@ -28,17 +30,41 @@ fi
 
 # Extract the image in the out-images folder
 if [ -d "${BUILD_DIR}/tmp/deploy/images" ]; then
-    cd ${BUILD_DIR}/tmp/deploy/images
-    cd *
-
+    # Create the output image folder
     if [ ! -d "${BUILD_DIR}/out-images" ]; then
         mkdir ${BUILD_DIR}/out-images
     fi
 
-    cp -L $1-*.rootfs.wic.bz2 ${BUILD_DIR}/out-images/root.img.bz2
+    # Enter the most recently modified folder
+    cd ${BUILD_DIR}/tmp/deploy/images
+    cd $(ls -rt | tail -n 1)
+
+    # Find the image file
+    IMG=$(ls -rt | grep "${1}-${PWD##*/}.rootfs-.*.wic.bz2" | tail -n 1)
+
+    if [ ! -z "${IMG}" ]; then
+        cp  ${IMG} ${BUILD_DIR}/out-images/${OUTPUT_IMG_NAME}.bz2
+    else
+        echo "ERROR: No output image found ('./scripts/buildImage.sh')"
+        exit 3
+    fi
+
+
+    # Decompress the image
     cd ${BUILD_DIR}/out-images
 
-    bzip2 -fd root.img.bz2 
+    # Store the pre decompression size
+    COMP_SIZE=$(du -h ${OUTPUT_IMG_NAME}.bz2 | cut -f 1)
+
+    echo "Decompressing the image"
+    bzip2 -fd ${OUTPUT_IMG_NAME}.bz2 
+
+    DECOMP_SIZE=$(du -h ${OUTPUT_IMG_NAME} | cut -f 1)
+
+    echo "Build completed, output stored in '${BUILD_DIR}/${OUTPUT_IMG_NAME}'"
+
+    echo "Compressed image size: ${COMP_SIZE}"
+    echo "Decompressed image size: ${DECOMP_SIZE}"
 else 
     echo "ERROR: No output image found ('./scripts/buildImage.sh')"
     exit 3
